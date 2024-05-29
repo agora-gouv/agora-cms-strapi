9a6eb29794c9bf673ef6bee411bf0a1a735d4091
ATMA <pheno62@gmail.com>
2024-05-29 14:22:47 +0200

    feat : ajout migration thématiques

---
+++ database/migrations-sql/2024-05-29-ajout-thematiques.sql
@@ -0,0 +1,95 @@
+INSERT INTO thematiques
+VALUES ('1', 'Travail', '💼', now(), now(), now(), 1, 1)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO thematiques
+VALUES ('2', 'Transition écologique', '🌱', now(), now(), now(), 1, 1)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO thematiques
+VALUES ('3', 'Santé', '🏥', now(), now(), now(), 1, 1)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO thematiques
+VALUES ('4', 'Economie', '📈', now(), now(), now(), 1, 1)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO thematiques
+VALUES ('5', 'Education & jeunesse', '🎓', now(), now(), now(), 1, 1)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO thematiques
+VALUES ('6', 'Europe & international', '🌏', now(), now(), now(), 1, 1)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO thematiques
+VALUES ('7', 'Transports', '🚊', now(), now(), now(), 1, 1)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO thematiques
+VALUES ('8', 'Sécurité & défense', '🛡', now(), now(), now(), 1, 1)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO thematiques
+VALUES ('9', 'Démocratie', '🗳', now(), now(), now(), 1, 1)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO thematiques
+VALUES ('10', 'Autre', '📦', now(), now(), now(), 1, 1)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO thematiques
+VALUES ('11', 'Agriculture & alimentation', '🌾', now(), now(), now(), 1, 1)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO thematiques
+VALUES ('12', 'Culture', '🎭', now(), now(), now(), 1, 1)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO thematiques
+VALUES ('13', 'Etudes sup. & recherche', '🧪', now(), now(), now(), 1, 1)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO thematiques
+VALUES ('14', 'Outre-mer', '🌍', now(), now(), now(), 1, 1)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO thematiques
+VALUES ('15', 'Justice', '⚖️', now(), now(), now(), 1, 1)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO thematiques
+VALUES ('16', 'Solidarités', '🤝', now(), now(), now(), 1, 1)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO thematiques
+VALUES ('17', 'Autonomie', '👵', now(), now(), now(), 1, 1)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO thematiques
+VALUES ('18', 'Handicap', '♿️', now(), now(), now(), 1, 1)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO thematiques
+VALUES ('19', 'Sport', '🏀', now(), now(), now(), 1, 1)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO thematiques
+VALUES ('20', 'Services publics', '🏛', now(), now(), now(), 1, 1)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO thematiques
+VALUES ('21', 'Energie', '⚡️', now(), now(), now(), 1, 1)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO thematiques
+VALUES ('22', 'Egalité', '👥️', now(), now(), now(), 1, 1)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO thematiques
+VALUES ('23', 'Enfance', '👶', now(), now(), now(), 1, 1)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO thematiques
+VALUES ('24', 'Logement', '🏡', now(), now(), now(), 1, 1)
+ON CONFLICT DO NOTHING;
