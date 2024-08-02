5480fbaa6bf1cfdad533583cbfde588a95698723
ATMA <pheno62@gmail.com>
2024-08-02 11:33:30 +0200

    feat : ajout headers, news et chart via Strapi

---
+++ src/api/qa-g-headers-onglet/services/qa-g-headers-onglet.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * qa-g-headers-onglet service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::qa-g-headers-onglet.qa-g-headers-onglet');
