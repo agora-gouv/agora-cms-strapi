e43f27e32d2603d1ac829b51b332c4b00907fcc3
Antoine Mazure <pheno62@gmail.com>
2025-04-01 16:11:04 +0200

    feat : bucket (#32)
    * feat : bucket
    
    * env security

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,17 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        baseUrl: env('CDN_URL'),
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: 'auto',
+        params: {
+          Bucket: env('AWS_BUCKET_NAME'),
+        },
+        endpoint: env('AWS_ENDPOINT'),
+      },
+    },
+  },
+});
