6909576334bab92b608fa3dabf269a61631350f8
@wojciech.wojcik <wojciech.wojcik@gmail.com>
2026-07-09 18:17:04 +0200

    ajout clusters de mots pour semaine libre

---
+++ src/api/cluster-semaine-libre/controllers/cluster-semaine-libre.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cluster-semaine-libre controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::cluster-semaine-libre.cluster-semaine-libre');
---
+++ src/api/cluster-semaine-libre/services/cluster-semaine-libre.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cluster-semaine-libre service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::cluster-semaine-libre.cluster-semaine-libre');
